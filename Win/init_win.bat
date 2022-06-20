04b2d5577217fb6f92bab41fdae61ebfa29fd6cd
Rubens Lyra <rubensvlyra@gmail.com>
2022-06-20 12:34:30 -0300

    init Windows app

---
+++ Win/init_win.bat
@@ -0,0 +1,29 @@
+cd C:\rlyra\Documents\Dev\Leia_TI\Win\
+
+# rem "O diretório não existe. Clonando projeto."
+	
+dotnet --version
+
+dotnet new mvc -o App -au none
+
+cd App\
+
+echo "Incluindo pacotes EF Core"
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore 
+dotnet add package Microsoft.EntityFrameworkCore.Relational 
+dotnet add package Microsoft.EntityFrameworkCore.Tools 
+		
+	
+touch logTarefa.txt 
+echo "Tarefa realizada em %date%, %time%" >> logTarefa.txt
+
+
+start logTarefa.txt 
+
+code .
+
+ls -la
+
+close .
